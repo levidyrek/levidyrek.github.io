69d18c980c828a46db80e80047b5bdb667de33cc
Levi Payne <levi.dyrek@gmail.com>
2016-09-09 19:00:57 -0400

    More progress on query.php

--- php/query.php
+++ php/query.php
@@ -1,9 +1,10 @@
 <?php
-	/** The purpose of this script is to query the database in a number of ways.
-	 This script should be called via AJAX. See below for supported actions and 
-	 required parameters for each.
+	/** 
+	The purpose of this script is to query the database in a number of ways. 
+	This script should be called via AJAX. See below for supported actions and 
+	required parameters for each.
 	 
-	 -------- ACTIONS -----------
+	 -------- ACTIONS ($_POST["action"])-----------
 	 
 	1. GET_TABLE: retrieve an entire table
 		- Parameters: 
@@ -34,7 +35,7 @@
 	**/
 	
 	// Constants
-	// 1. Actions
+	// Actions
 	define("GET_TABLE", 100); 
 	define("UPDATE_TABLE", 101);
 	define("SELECT_TABLE", 102); 
@@ -49,11 +50,37 @@
 	$conn = new mysqli($server, $username, $password, $db_name);
 	if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
 	
-	$id = $_POST["id"];
-	$q = "SELECT * FROM projects WHERE id='$id'";
-	
-	$result = $conn->query($q);
-	if ($result->num_rows > 0) {
-		echo json_encode($result->fetch_assoc());
+	// Action must be set
+	isset($_POST["action"]) or die("Error: POST variable 'action' must be set.");
+
+	// Table name must be given for all actions
+	isset($_POST["table_name"]) or die("Error: POST variable 'table_name' must be set");
+	$table = $_POST["table_name"];
+
+	// See which action needs to be done
+	switch ($_POST["action"]) {
+		case GET_TABLE:
+			$q = "SELECT * FROM '$table'";
+			$result = $conn->query($q);
+			if ($result->num_rows > 0) {
+				while ($row = $result->fetch_assoc()) {
+					
+				}
+			}
+			break;
+		case UPDATE_TABLE:
+
+			break;
+		case SELECT_TABLE:
+
+			break;
+		case ADD_ROW:
+
+			break;
+		case REMOVE_ROW:
+
+			break;
+		default:
+			die("Error: POST variable 'action' has an unknown value.");
 	}
 ?>
