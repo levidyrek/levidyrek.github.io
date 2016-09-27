947b50429692a56830f519398ce9378f6b984c89
levidyrek <levi.dyrek@gmail.com>
2016-09-27 18:27:32 -0400

    Starting on a redesign with a dark background, centered around the about page.

--- about.php
+++ about.php
@@ -8,40 +8,43 @@
 </head>
 <body>
 	<div id="nav-container"></div>
-	<div class="main container">
+	<div class="main container-fluid">
 		<div class="row">
-			<div class="col-md-12">
+			<div class="col-md-3 col-sm-offset-1 self-container">
 				<img id="self" src="images/self.jpeg" alt="self-portrait">
 			</div>	
-		</div>
-		<div class="row">
-			<div class="col-md-10 col-md-offset-1">
-				<div class="bio">
-					<h3>Bio</h3>
-					<p>Hi! I'm a recent Computer Science graduate from the University of Virginia. I am very passionate about technology and software development, and I am very enthusiastic about what I do.</p>
+				<div class="col-md-4">
+					<div class="row">
+						<div class="bio">
+							<h3>bio <span class="bracket">{</span></h3>
+							<div class="indent-1">
+								<p>Hi! I'm a recent Computer Science graduate from the University of Virginia. I am very passionate about technology and software development, and I am very enthusiastic about what I do.</p>
+							</div>
+							<h3 class="bracket">}</h3>
+						</div>
+					</div>
+					<div class="row">
+						<div class="education">
+							<h3>education <span class="bracket">{</span></h3>
+							<div class="indent-1">
+								<h5>University of Virginia (2014-2016)</h5>
+								<ul>
+									<li>B.A. in Computer Science</li>
+									<li>GPA: 3.665</li>
+									<li>Major GPA: 3.883</li>
+								</ul>
+								<h5>Wytheville Community College (2013-2014)</h5>
+								<ul>
+									<li>A.A.S. in General Studies</li>
+									<li>Member of Phi Theta Kappa</li>
+									<li>GPA: 3.8</li>
+								</ul>
+							</div>
+							<h3 class="bracket">}</h3>
+						</div>
+					</div>
 				</div>
-			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-10 col-md-offset-1">
-				<div class="education">
-					<h3>Education</h3>
-					<h5><b>University of Virginia (2014-2016)</b></h5>
-					<ul>
-						<li>Bachelor of Arts in Computer Science</li>
-						<li>GPA: 3.665</li>
-						<li>Major GPA: 3.883</li>
-					</ul>
-					<h5><b>Wytheville Community College (2013-2014)</b></h5>
-					<ul>
-						<li>Associate of Arts and Sciences in General Studies</li>
-						<li>Member of Phi Theta Kappa</li>
-						<li>GPA: 3.8</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<hr>
 		<div class="row">
 			<div class="col-md-10 col-md-offset-1">
 				<h1 id="skills-header">Skills</h1>
@@ -129,20 +132,20 @@
 		</div>
 	</div>
 
-			<!--Scripts-->
-			<script src="js/navbar.js"></script>
-			<script src="js/about.js"></script>
-			<!-- Google Analytics -->
-			<script>
-				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-					(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-					m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	<!--Scripts-->
+	<script src="js/navbar.js"></script>
+	<script src="js/about.js"></script>
+	<!-- Google Analytics -->
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-				ga('create', 'UA-63481408-1', 'auto');
-				ga('send', 'pageview');
+	ga('create', 'UA-63481408-1', 'auto');
+	ga('send', 'pageview');
 
-			</script>
+	</script>
 
-		</body>
-		</html>  
+</body>
+</html>  
